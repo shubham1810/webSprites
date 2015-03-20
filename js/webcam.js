106ddd644c0b529368273844654b370d1c603b04
Shubham Dokania <skd.1810@gmail.com>
2015-03-20 13:36:35 +0530

    first blood

---
+++ js/webcam.js
@@ -0,0 +1,18 @@
+if(navigator.webkitGetUserMedia!=null) {
+    var options = {
+        video:true,
+        audio:false
+    };
+
+    navigator.webkitGetUserMedia(options,
+        function(stream) {
+            var video = document.
+                querySelector('video');
+            video.src = window.webkitURL.
+                createObjectURL(stream);
+        },
+        function(e) {
+            console.log("There was a problem with the webkitUserMedia");
+        }
+    );
+}
